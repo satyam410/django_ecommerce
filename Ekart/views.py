d2a655e522edd1c9ffdafee5d92580d1070dd775
satyam410 <satyamsinghnoute@gmail.com>
2023-07-26 00:05:59 +0530

    first commit

---
+++ Ekart/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from store.models import Product
+
+def home(request):
+    products = Product.objects.all().filter(is_available=True)
+    product_count = products.count()
+    context = {
+        'products' : products,
+        'product_count' : product_count
+    }
+    return render(request,'index.html',context)
---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import include, path
+
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('<slug:category_slug>/',views.store,name = 'product_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/',views.product_detail,name = 'product_details')
+]
